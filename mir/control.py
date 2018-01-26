46b1ce9b934ab8b82d89a9cf6c3810c8faca153f
Manoel Vilela <manoel_vilela@engineer.com>
2018-01-26 06:05:29 -0300

    Add basic functionally as multimedia remote control
    + Volume control / Mute
    + PlayBack, Next Song, Previous etc
    + Shutdown the computer and reboot
    + Email, Browser and another cancer stuff

--- mir/control.py
+++ mir/control.py
@@ -5,11 +5,15 @@
 # third-library
 import serial
 from serial.tools.list_ports import comports as list_ports
-import pykeyboard
-import pymouse
+from pymouse import PyMouse
+from pykeyboard import PyKeyboard
+
+m = PyMouse()
+k = PyKeyboard()
 
 # self-package
-import mir.keys
+import mir.keys as keys
+import mir.x as X
 
 BAUDRATE = 9600
 
@@ -29,12 +33,37 @@ def interpreter(signal):
     "Given a signal 8-digit hex encoded integer, make a action"
     print("SIGNAL: {:08X}".format(signal))
 
+    if signal == keys.VOL_UP:
+        k.tap_key(X.VOL_DOWN)
+    elif signal == keys.VOL_DOWN:
+        k.tap_key(X.VOL_UP)
+    elif signal == keys.MUTE:
+        k.tap_key(X.MUTE)
+    elif signal == keys.RED:
+        k.tap_key(X.SHUTDOWN)
+    elif signal == keys.CH_UP:
+        k.tap_key(X.NEXT_SONG)
+    elif signal == keys.CH_DOWN:
+        k.tap_key(X.PREVIOUS_SONG)
+    elif signal == keys.CONFIRM:
+        k.tap_key(X.PLAY_PAUSE)
+    elif signal == keys.IBUTTON:
+        k.tap_key(X.WWW)
+    elif signal == keys.MATRIX:
+        k.tap_key(X.MAIL)
+    elif signal == keys.YELLOW:
+        k.tap_key(X.REBOOT_SESSION)
+    else:
+        print("Nothing to do.")
+
 
 def main():
     "Main procedure of this fucking program"
     port = list_ports()[0]
     conn = connect(port.device)
+    print("Connected at {}".format(port.device))
     stream = stream_connection(conn)
+    print("Streaming started.")
     for signal in stream:
         interpreter(signal)
 
