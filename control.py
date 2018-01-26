cb3f5e7e9103e28c513aa0e7129828123fd0dff3
Manoel Vilela <manoel_vilela@engineer.com>
2018-01-26 03:27:46 -0300

    Add skeleton of control.py code
    This will make things more easy to abstract.

---
+++ control.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# coding=utf-8
+
+
+import serial
+import pykeyboard
+import pymouse
+
+
+def connect(port):
+    "Connect the serial port and return connection"
+    pass
+
+
+def stream(conn):
+    "Make a stream of signals based on the conn (connection)"
+    pass
+
+
+def interpreter(signal):
+    "Given a signal 8-digit hex encoded integer, make a action"
+    pass
+
+
+def main():
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
