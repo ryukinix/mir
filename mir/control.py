eac090ad41f66cd0818326675e28e6c35c9dee05
Manoel Vilela <manoel_vilela@engineer.com>
2018-01-26 04:46:46 -0300

    Add connection and streaming signal at mir/control.py
    Now we can look the signals at the terminal. I need implement
    further the interpretation of the commands by using mouse/keyboard
    from PyUserInput.

--- mir/control.py
+++ mir/control.py
@@ -2,30 +2,45 @@
 # coding=utf-8
 
 
+# third-library
 import serial
+from serial.tools.list_ports import comports as list_ports
 import pykeyboard
 import pymouse
+
+# self-package
+import mir.keys
+
+BAUDRATE = 9600
 
 
 def connect(port):
     "Connect the serial port and return connection"
-    pass
+    return serial.Serial(port, BAUDRATE)
 
 
-def stream(conn):
-    "Make a stream of signals based on the conn (connection)"
-    pass
+def stream_connection(conn):
+    "Make a stream of signals based on the conn (serial connection)"
+    while True:
+        yield int(conn.readline().decode('utf-8').strip(), 16)
 
 
 def interpreter(signal):
     "Given a signal 8-digit hex encoded integer, make a action"
-    pass
+    print("SIGNAL: {:08X}".format(signal))
 
 
 def main():
-    pass
-
+    "Main procedure of this fucking program"
+    port = list_ports()[0]
+    conn = connect(port.device)
+    stream = stream_connection(conn)
+    for signal in stream:
+        interpreter(signal)
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nShutdown. Bye!")
